02f87f2114954f2d9fa612245fccfc57bf838897
chinthu jemsheed <chinthu111@gmail.com>
2025-03-10 12:34:33 +0100

    changed

--- test/test.sh
+++ test/test.sh
@@ -4,9 +4,9 @@ kubectl apply -f fluent-bit-test-job.yaml
 kubectl wait --for=condition=complete job/fluent-bit-test-log -n zenoh-monitoring --timeout=60s
 
 kubectl apply -f loki-query-test.yaml
-kubectl wait --for=condition=complete job/loki-query-test -n zenoh-monitoring --timeout=60s
+kubectl wait --for=condition=complete job/loki-query-job -n zenoh-monitoring --timeout=60s
 
-LOKI_TEST_RESULT=$(kubectl logs job/loki-query-test -n zenoh-monitoring)
+LOKI_TEST_RESULT=$(kubectl logs job/loki-query-job -n zenoh-monitoring)
 
 if [[ "$LOKI_TEST_RESULT" == *"Loki test passed"* ]]; then
   echo "Fluent Bit to Loki test passed"
